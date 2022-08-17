e4c89b7dc19a89e165b27a453d78f4f883d5307d
Tanay Mehta <mehtatanay30@gmail.com>
2022-08-17 20:01:03 +0530

    twosum array problem

---
+++ twoSum.js
@@ -0,0 +1,10 @@
+var twoSum = function(nums, target) {
+    let i=0,arr= [...nums]
+    while(arr.length > 1){
+       let n = arr.shift()
+       if(arr.indexOf(target-n) != -1){
+           i = nums.indexOf(n)
+           return [nums.indexOf(n), nums.indexOf(target - n , i+1)]
+       }
+    }
+};
